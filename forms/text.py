12607c3faed2ca9d2c5f01efbbfa06fe22315ae9
Maksim <maksimf647@gmail.com>
2024-04-14 12:42:24 +0300

    updated

--- forms/text.py
+++ forms/text.py
@@ -2,9 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField
 from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired
+from sqlalchemy_serializer import SerializerMixin
 
 
-class SendTextForm(FlaskForm):
+class SendTextForm(FlaskForm, SerializerMixin):
     text = TextAreaField("Your text:", validators=[DataRequired()])
     picture = FileField("Изображение", validators=[FileAllowed(['jpg', 'png'],
                                                                'Загрузить можно только файлы jpg, png')])
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,3 +3,15 @@ function handleImageUpload(event) {
     image.src = URL.createObjectURL(event.target.files[0]);
     image.style.display = 'block';
 }
+
+function Copy(containerid) {
+	    let textarea = document.createElement('textarea');
+	    textarea.id = 'temp';
+	    textarea.style.height = 0;
+	    document.body.appendChild(textarea);
+	    textarea.value = document.getElementById(containerid).innerText;
+	    let selector = document.querySelector('#temp');
+	    selector.select();
+	    document.execCommand('copy');
+	    document.body.removeChild(textarea);
+}
